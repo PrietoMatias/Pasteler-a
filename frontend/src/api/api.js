b6c037a11ba6af49dbf4739aef4871e1343dec1b
augustoterrera <augustoterrera@gmail.com>
2024-11-24 15:33:18 -0300

    Direccion del back

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-    baseURL:'http://localhost:3000/api',
+    baseURL:'https://pasteleiabackend.onrender.com/api',
     timeout: 5000,
     headers:{
         'Content-Type': 'application/json'
