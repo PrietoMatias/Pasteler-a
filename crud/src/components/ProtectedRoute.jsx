7ec0a8704da879ace57108dc464f46c01820882d
PrietoMatias <matiashernan132@gmail.com>
2024-11-24 18:50:10 -0300

    sacando protección de rutas

--- crud/src/components/ProtectedRoute.jsx
+++ crud/src/components/ProtectedRoute.jsx
@@ -1,16 +1,9 @@
 import { Navigate } from 'react-router-dom';
-import useAuth from '../hooks/useAuth.js';
 
 const ProtectedRoute = ({ children }) => {
-  const { isAuthenticated, loading } = useAuth();
-  console.log(isAuthenticated);
 
   if (loading) {
     return <div>Loading...</div>; 
-  }
-
-  if (isAuthenticated) {
-    return <Navigate to="/" />;
   }
 
   return children;
