17db5bce376d3088ec13bb0acf38bd2957ed4146
augustoterrera <augustoterrera@gmail.com>
2024-11-25 18:35:52 -0300

    Cambiamos link de back

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-    baseURL:'https://pasteleiabackend.onrender.com/api',
+    baseURL:'https://pasteleria-backend.onrender.com/api',
     timeout: 5000,
     headers:{
         'Content-Type': 'application/json'
